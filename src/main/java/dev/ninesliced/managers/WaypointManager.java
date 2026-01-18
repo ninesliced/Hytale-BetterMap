1380d773fd3f05e7db7d21a0aafc2479b2e2e247
Louis Rollet <louis.rollet@epitech.eu>
2026-01-18 16:16:29 +0800

    [FIX] issue that waypoint doesn't update properly when deleted

--- src/main/java/dev/ninesliced/managers/WaypointManager.java
+++ src/main/java/dev/ninesliced/managers/WaypointManager.java
@@ -120,10 +120,10 @@ public class WaypointManager {
         }
 
         if (found) {
-            perWorldData.setWorldMapMarkers(newMarkerList.toArray(new MapMarker[0]));
             persistPersonal(player, world.getName(), newMarkerList);
             UUID playerUuid = ((CommandSender) player).getUuid();
             loadedPlayers.remove(playerUuid);
+            ensureLoaded(player, world);
         }
         return found;
     }
@@ -167,10 +167,10 @@ public class WaypointManager {
             return false;
         }
 
-        perWorldData.setWorldMapMarkers(rebuilt.toArray(new MapMarker[0]));
         persistPersonal(player, world.getName(), rebuilt);
         UUID playerUuid = ((CommandSender) player).getUuid();
         loadedPlayers.remove(playerUuid);
+        ensureLoaded(player, world);
         return true;
     }
 
